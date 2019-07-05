6b4b71627104ba1a59dca9ff7c9c417978cf1f9a
Hanna Engel <hengel@alumni.berklee.edu>
2019-07-05 14:02:32 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
-// create your App component here
+import React, { Component } from 'react'
+//import other files here
+
+export default class App extends Component {
+
+    state = {people: []}
+
+componentDidMount(){
+    fetch('http://api.open-notify.org/astros.json')
+        .then(res=>res.json())
+        .then(data => this.setState({people: data.people}))  
+}
+    render() {
+        return( 
+            <ul>
+                {this.state.people.map((person, index) => {
+                    return <li key={index}>{person.name}</li>
+                 })}
+            </ul>
+        )
+    }
+}
